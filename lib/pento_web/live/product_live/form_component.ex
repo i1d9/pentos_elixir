2620341f9e5d078d1f11d12a5457d05910ddb523
ian <highmeme10@gmail.com>
2021-10-17 05:46:25 +0300

    Liveview image upload works

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -10,7 +10,61 @@ defmodule PentoWeb.ProductLive.FormComponent do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image, 
+        accept: ~w(.jpg .jpeg .png),
+        max_entries: 1,
+        auto_upload: true,
+        progress: &handle_progress/3)
+      }
+  end
+
+  @doc """
+  Invoked on the allow_upload function on the socket
+  Sends a progress event for each bit sent
+
+  The first argument is the name of the upload field
+  The second argument is the file being uploaded 
+  The third argument is the socket connection
+
+  If the upload is done consume_uploaded_entry/3 is invoked
+  consume_uploaded_entry takes the socket, the file being uploaded and a callback function
+  """
+  defp handle_progress(:image, entry, socket) do
+    # :timer.sleep(1000)
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> update_changeset(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @doc """
+  Extracts the changeset from the socket connection
+  Add the image_upload attribute to the changeset
+  """
+  def update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
+    socket
+    |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
+  end
+
+  @doc """
+  Interpolates the static directory with the filename
+  Returns the route of the uploaded image
+  """
+  defp upload_static_file(%{path: path}, socket) do
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    Routes.static_path(socket, "/images/#{Path.basename(dest)}")
   end
 
   @impl true
