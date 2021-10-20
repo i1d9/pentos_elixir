27f48681aa0bbc6bd2ea8920c06385ed9567aee2
ian <highmeme10@gmail.com>
2021-10-20 15:50:13 +0300

    form_component rewrite

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -1,4 +1,3 @@
-
 defmodule PentoWeb.ProductLive.FormComponent do
   use PentoWeb, :live_component
 
@@ -7,66 +6,17 @@ defmodule PentoWeb.ProductLive.FormComponent do
   @impl true
   def update(%{product: product} = assigns, socket) do
     changeset = Catalog.change_product(product)
-
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:changeset, changeset)
-     |> allow_upload(:image,
+    {:ok, socket
+      |> assign(assigns)
+      |> assign(:changeset, changeset)
+      |> allow_upload(:image,
         accept: ~w(.jpg .jpeg .png),
         max_entries: 1,
         auto_upload: true,
-        progress: &handle_progress/3)
-      }
+        progress: &handle_progress/3
+      )}
   end
 
-  @doc """
-  Invoked on the allow_upload function on the socket
-  Sends a progress event for each bit sent
-
-  The first argument is the name of the upload field
-  The second argument is the file being uploaded
-  The third argument is the socket connection
-
-  If the upload is done consume_uploaded_entry/3 is invoked
-  consume_uploaded_entry takes the socket, the file being uploaded and a callback function
-  """
-  defp handle_progress(:image, entry, socket) do
-    # :timer.sleep(1000)
-    if entry.done? do
-      path =
-        consume_uploaded_entry(
-          socket,
-          entry,
-          &upload_static_file(&1, socket)
-        )
-      {:noreply,
-       socket
-       |> put_flash(:info, "file #{entry.client_name} uploaded")
-       |> update_changeset(:image_upload, path)}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  @doc """
-  Extracts the changeset from the socket connection
-  Add the image_upload attribute to the changeset
-  """
-  def update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
-    socket
-    |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
-  end
-
-  @doc """
-  Interpolates the static directory with the filename
-  Returns the route of the uploaded image
-  """
-  defp upload_static_file(%{path: path}, socket) do
-    dest = Path.join("priv/static/images", Path.basename(path))
-    File.cp!(path, dest)
-    Routes.static_path(socket, "/images/#{Path.basename(dest)}")
-  end
 
   @impl true
   def handle_event("validate", %{"product" => product_params}, socket) do
@@ -80,6 +30,35 @@ defmodule PentoWeb.ProductLive.FormComponent do
 
   def handle_event("save", %{"product" => product_params}, socket) do
     save_product(socket, socket.assigns.action, product_params)
+  end
+
+
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :image, ref)}
+  end
+
+  defp handle_progress(:image, entry, socket) do
+    # :timer.sleep(1000)
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      {:noreply,
+       socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> update_changeset(:image_upload, path)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  def update_changeset(%{assigns: %{changeset: changeset}} = socket, key, value) do
+    socket
+    |> assign(:changeset, Ecto.Changeset.put_change(changeset, key, value))
   end
 
   defp save_product(socket, :edit, product_params) do
@@ -106,5 +85,35 @@ defmodule PentoWeb.ProductLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    # Plug in your production image file persistence implementation here!
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    Routes.static_path(socket, "/images/#{Path.basename(dest)}")
+  end
+
+  def upload_image_error(%{image: %{errors: errors}}, entry) when length(errors) > 0 do
+    {_, msg} =
+      Enum.find(errors, fn {ref, _} ->
+        ref == entry.ref || ref == entry.upload_ref
+      end)
+
+    upload_error_msg(msg)
+  end
+
+  def upload_image_error(_, _), do: ""
+
+  defp upload_error_msg(:not_accepted) do
+    "Invalid file type"
+  end
+
+  defp upload_error_msg(:too_many_files) do
+    "Too many files"
+  end
+
+  defp upload_error_msg(:too_large) do
+    "File exceeds max size"
   end
 end
